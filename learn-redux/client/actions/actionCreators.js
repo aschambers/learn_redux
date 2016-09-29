cfc0e138890359b6ebc598bf8c10750c13dd6a92
Alan Schambers <aschambers4291@gmail.com>
2016-09-28 19:05:52 -0700

    add reducers

--- learn-redux/client/actions/actionCreators.js
+++ learn-redux/client/actions/actionCreators.js
@@ -1,11 +1,11 @@
-// increment likes
+// increment likes, all you need is the identifier, want to send as little info as possible, could be postId as well.
 export function increment(index) {
 	return {
 		type: 'INCREMENT_LIKES',
 		index: index
 	}
 }
-// add comment
+// add comment, need to know what postId, author who commented, and what comment they made!
 export function addComment(postId, author, comment) {
 	return {
 		type: 'ADD_COMMENT',
---
+++ learn-redux/client/reducers/posts.js
@@ -0,0 +1,12 @@
+// reducer takes in two things:
+
+// 1. action (info about what happened).
+// 2. copy of current state
+
+// set state to be an empty array.
+function posts(state = [], action) {
+	console.log(state, action);
+	return state;
+}
+
+export default posts;
